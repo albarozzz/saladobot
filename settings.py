0bfe7ee255f2c3e28e3ed9042cc2cf47b7229f67
albarozzz <alvaroElps4@gmail.com>
2022-03-10 19:01:15 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+# twitter authentication keys
+consumer_key = ""
+consumer_secret = ""
+key = ""
+secret = ""
+# ---------------------------
+
+# google custom search api
+google_api = ""
+cx = ""
+# ---------------------------
